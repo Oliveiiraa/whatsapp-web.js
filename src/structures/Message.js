3979f785f42e903e6c61faf74aec6694502924c0
André Menezes <menezesbr@gmail.com>
2023-04-11 09:25:39 -0300

    "version": "1.19.10"

--- src/structures/Message.js
+++ src/structures/Message.js
@@ -109,7 +109,7 @@ class Message extends Base {
          * Indicates if the message is a status update
          * @type {boolean}
          */
-        this.isStatus = data.isStatusV3;
+        this.isStatus = data.isStatusV3 || data.id.remote === 'status@broadcast';
 
         /**
          * Indicates if the message was starred
@@ -444,15 +444,14 @@ class Message extends Base {
      */
     async delete(everyone) {
         await this.client.pupPage.evaluate((msgId, everyone) => {
-            const msg = window.Store.Msg.get(msgId);
-            const chat = msg && (msg.chat || window.Store.Chat.get(msg.id.remote._serialized));
+            let msg = window.Store.Msg.get(msgId);
 
             const canRevoke = window.Store.MsgActionChecks.canSenderRevokeMsg(msg) || window.Store.MsgActionChecks.canAdminRevokeMsg(msg);
             if (everyone && canRevoke) {
-                return window.Store.Cmd.sendRevokeMsgs(chat, [msg], { type: msg.id.fromMe ? 'Sender' : 'Admin' });
+                return window.Store.Cmd.sendRevokeMsgs(msg.chat, [msg], { type: msg.id.fromMe ? 'Sender' : 'Admin' });
             }
 
-            return window.Store.Cmd.sendDeleteMsgs(chat, [msg], true);
+            return window.Store.Cmd.sendDeleteMsgs(msg.chat, [msg], true);
         }, this.id._serialized, everyone);
     }
 
